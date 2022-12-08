e88e33b0240ec66b65d756437e5e39d283364e38
Danail <d_krasimirov@yahoo.com>
2022-12-08 21:19:32 +0100

    rand commit

--- some-func/randompassgen/main.go
+++ some-func/randompassgen/main.go
@@ -1,58 +1,58 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"math/rand"
-	"os"
-	"strconv"
-	"time"
-)
+// import (
+// 	"fmt"
+// 	"math/rand"
+// 	"os"
+// 	"strconv"
+// 	"time"
+// )
 
-var MIN = 0
-var MAX = 94
+// var MIN = 0
+// var MAX = 94
 
-func random(min, max int) int {
-	return rand.Intn(max-min) + min
-}
+// func random(min, max int) int {
+// 	return rand.Intn(max-min) + min
+// }
 
-func getString(len int64) string {
-	temp := ""
-	startChar := "!"
+// func getString(len int64) string {
+// 	temp := ""
+// 	startChar := "!"
 
-	var i int64 = 1
+// 	var i int64 = 1
 
-	for {
-		myRand := random(MIN, MAX)
-		newChar := string(startChar[0] + byte(myRand))
-		temp = temp + newChar
-		if i == len {
-			break
-		}
-		i++
-	}
+// 	for {
+// 		myRand := random(MIN, MAX)
+// 		newChar := string(startChar[0] + byte(myRand))
+// 		temp = temp + newChar
+// 		if i == len {
+// 			break
+// 		}
+// 		i++
+// 	}
 
-	return temp
-}
+// 	return temp
+// }
 
-func main() {
+// func main() {
 
-	var LENGTH int64 = 8
+// 	var LENGTH int64 = 8
 
-	arguments := os.Args
-	switch len(arguments) {
-	case 2:
-		t, err := strconv.ParseInt(os.Args[1], 10, 64)
-		if err == nil {
-			LENGTH = t
-		}
-		if LENGTH <= 0 {
-			LENGTH = 8
-		}
-	default:
-		fmt.Println("Using default values...")
-	}
+// 	arguments := os.Args
+// 	switch len(arguments) {
+// 	case 2:
+// 		t, err := strconv.ParseInt(os.Args[1], 10, 64)
+// 		if err == nil {
+// 			LENGTH = t
+// 		}
+// 		if LENGTH <= 0 {
+// 			LENGTH = 8
+// 		}
+// 	default:
+// 		fmt.Println("Using default values...")
+// 	}
 
-	SEED := time.Now().Unix()
-	rand.Seed(SEED)
-	fmt.Println(getString(LENGTH))
-}
+// 	SEED := time.Now().Unix()
+// 	rand.Seed(SEED)
+// 	fmt.Println(getString(LENGTH))
+// }
