307d9fd12e04985d9f308dc8e2c47bb77e58d2c9
Danail Surudzhiyski <dans@bankingcircle.com>
2022-12-30 13:36:55 +0100

    adding todofiber

---
+++ todofiber/cmd/todofiber/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/gofiber/template/html"
+
+	"github.com/dkr290/go-devops/todofiber/handlers"
+	"github.com/dkr290/go-devops/todofiber/repository"
+	"github.com/gofiber/fiber/v2"
+	_ "github.com/lib/pq"
+)
+
+var Repo = repository.NewRepo()
+
+func main() {
+
+	engine := html.New("./views", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+	port := os.Getenv("APP_PORT")
+
+	connInfo := "user=postgres password=Password123 host=172.19.158.144 sslmode=disable"
+	initdb, err := sql.Open("postgres", connInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbName := "todos"
+	_, err = initdb.Exec("create database " + dbName)
+	if strings.Contains(err.Error(), "already exists") && err != nil {
+		//handle the error
+		fmt.Println("Database already created")
+
+	} else {
+		log.Fatalln("Cannot create database", err)
+	}
+	initdb.Close()
+
+	connStr := "postgresql://postgres:Password123@172.19.158.144/todos?sslmode=disable"
+	// Connect to database
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//Then execute your query for creating table
+	_, err = db.Exec(fmt.Sprintf("CREATE TABLE todos( %s )", "item text"))
+
+	if strings.Contains(err.Error(), "already exists") && err != nil {
+		fmt.Println("Table already created")
+	} else {
+		log.Fatal("Create table failed ", err)
+	}
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return handlers.IndexHandler(db, c)
+	})
+	app.Post("/", func(c *fiber.Ctx) error {
+		return handlers.PostHandler(db, c)
+	})
+	app.Put("/update", func(c *fiber.Ctx) error {
+		return handlers.UpdateHandler(db, c)
+	})
+	app.Delete("/delete", func(c *fiber.Ctx) error {
+		return handlers.DeleteHandler(db, c)
+	})
+
+	if port == "" {
+		port = "3000"
+	}
+
+	app.Static("/", "./public")
+	log.Fatalln(app.Listen(fmt.Sprintf(":%v", port)))
+}
