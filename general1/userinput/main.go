0af72647e8cc258f600405c0291184e0380f4019
dkrasimirov <dkrasimirov@example.com>
2023-03-02 22:23:46 +0100

    commit some more examples

--- general1/userinput/main.go
+++ general1/userinput/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"reflect"
+	"strconv"
 )
 
 func main() {
@@ -21,5 +23,27 @@ func main() {
 	}
 
 	fmt.Println("Hello", name)
+
+	//some conversion
+
+	cv1 := 1.5
+
+	cv2 := int(cv1)
+	fmt.Printf("THe %d and type %v\n", cv2, reflect.TypeOf(cv2))
+
+	cv3 := "5000000"
+
+	cv4, err := strconv.Atoi(cv3)
+	if err != nil {
+		log.Fatal("Error conversion string", err)
+	}
+
+	fmt.Printf("The value is %d  and type %v\n", cv4, reflect.TypeOf(cv4))
+
+	cv7 := 500000000
+
+	cv8 := strconv.Itoa(cv7)
+
+	fmt.Println("The string is", cv8)
 
 }
